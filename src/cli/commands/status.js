2dc26a74957196adc3fbc24973e01b5176147977
Typicode <typicode@gmail.com>
2014-09-25 03:13:17 +0200

    Syntax

--- src/cli/commands/status.js
+++ src/cli/commands/status.js
@@ -4,27 +4,27 @@ var sh      = require('shelljs')
 var config  = require('../../config.js')
 
 module.exports = function() {
+  console.log('Checking that katon daemon is loaded...')
 
-    console.log('Checking that katon daemon is loaded...')
+  var output = sh.exec('launchctl list | grep \'katon\'', { silent: true }).output
+  daemonLoaded = output.indexOf('katon\n') !== -1
 
-    var output = sh.exec('launchctl list | grep \'katon\'', { silent: true }).output
-    daemonLoaded = output.indexOf('katon\n') !== -1
+  if (daemonLoaded) {
+    console.log(chalk.green('OK'))
+  } else {
+    return console.log(chalk.red('KO daemon is not loaded, try `katon start`'))
+  }
 
-    if (daemonLoaded) {
-      console.log(chalk.green('OK'))
+  console.log('Checking .ka domain...')
+  
+  request({
+    url: 'http://katon-status-test.ka',
+    timeout: 1000 
+  }, function(err) {
+    if (err) {
+      console.log(chalk.red('KO try `sudo katon install`'))
     } else {
-      return console.log(chalk.red('KO daemon is not loaded, try `katon start`'))
+      console.log(chalk.green('OK'))
     }
-
-    console.log('Checking .ka domain...')
-    request({
-      url: 'http://katon-status-test.ka',
-      timeout: 1000 
-    }, function(err) {
-      if (err) {
-        console.log(chalk.red('KO try `sudo katon install`'))
-      } else {
-        console.log(chalk.green('OK'))
-      }
-    })
+  })
 }
