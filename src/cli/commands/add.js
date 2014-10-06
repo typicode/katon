d171c015b8cf6077e5a3d9b1fca3252584330b42
Typicode <typicode@gmail.com>
2014-10-06 11:57:00 +0200

    Add --env option to add command

--- src/cli/commands/add.js
+++ src/cli/commands/add.js
@@ -1,31 +1,59 @@
 var fs         = require('fs')
+var minimist   = require('minimist')
 var mkdirp     = require('mkdirp')
 var chalk      = require('chalk')
 var pathToHost = require('../utils/path-to-host')
 var config     = require('../../config')
 
-// add <cmd> [name]
+// add <cmd> [name] -e [ENV] -e [ENV]
 module.exports = function(args) {
+  // Make sure hosts dir exists
   mkdirp.sync(config.hostsDir)
 
-  var command = args[0] ? args[0].trim() : ''
+  // Parse args
+  args = minimist(args, {
+    string: ['env'],
+    alias: { e: 'env' }
+  })
+
+  // Get command
+  var command = args._[0] ? args._[0].trim() : ''
 
   if (command === '') {
     return console.log(
         'Please specify a command\n'
       + 'katon add <command>'
     )
-  } 
+  }
 
-  var host = args[1] ? args[1] : pathToHost(pathToHost(process.cwd()))
+  // Get host name
+  var host = args._[1] ? args._[1] : pathToHost(pathToHost(process.cwd()))
 
+  // Create env
+  var env = {
+    PATH: process.env.PATH
+  }
+
+  if (typeof args.env === 'string') args.env = [args.env]
+
+  for (var i in args.env) {
+    var e = args.env[i]
+
+    if (e === 'PATH') continue
+
+    if (process.env.hasOwnProperty[e]) {
+      env[e] = process.env[e]
+    } else {
+      return console.log('Can\'t find ' + e + ' environment variable')
+    }
+  }
+
+  // Create host file
   fs.writeFileSync(config.hostsDir + '/' + host + '.json', JSON.stringify(
     {
       command : command,
       cwd     : process.cwd(),
-      env     : {
-        PATH: process.env.PATH
-      }
+      env     : env
     }
     , null, 2)
   )
