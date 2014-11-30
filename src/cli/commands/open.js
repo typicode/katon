69dd8a695afb850a275c51d6ec40c089ef3bca9f
Typicode <typicode@gmail.com>
2014-11-30 07:16:21 +0100

    Add color to open command

--- src/cli/commands/open.js
+++ src/cli/commands/open.js
@@ -1,12 +1,13 @@
 var path       = require('path')
 var open       = require('opn')
+var chalk      = require('chalk')
 var pathToHost = require('../utils/path-to-host')
 
 // open [name]
 module.exports = function(args) {
   var host = args[0] || pathToHost(process.cwd())
   var url  = 'http://' + host + '.ka/'
-  
-  console.log('Opening ' + url)
+
+  console.log('Opening', chalk.cyan(url))
   open(url)
 }
