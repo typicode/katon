ef3344db2bdcaa69ecbd313b34fffe827c453398
Typicode <typicode@gmail.com>
2015-03-04 18:07:13 +0100

    Add --https option to katon open

--- src/cli/commands/open.js
+++ src/cli/commands/open.js
@@ -1,12 +1,15 @@
 var path       = require('path')
+var minimist   = require('minimist')
 var open       = require('opn')
 var chalk      = require('chalk')
 var pathToHost = require('../utils/path-to-host')
 
-// open [name]
+// open [name] --https
 module.exports = function(args) {
-  var host = args[0] || pathToHost(process.cwd())
-  var url  = 'http://' + host + '.ka/'
+  var args = minimist(args)
+  var host = args._[0] || pathToHost(process.cwd())
+  var protocol = args.https ? 'https' : 'http'
+  var url  = protocol + '://' + host + '.ka/'
 
   console.log('Opening', chalk.cyan(url))
   open(url)
