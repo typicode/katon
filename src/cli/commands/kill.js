aecd51af2c277e546bd75175bfcaa29b1d23f182
Typicode <typicode@gmail.com>
2015-03-07 12:14:30 +0100

    Update kill command

--- src/cli/commands/kill.js
+++ src/cli/commands/kill.js
@@ -1,15 +1,19 @@
-var fs     = require('fs')
-var path   = require('path')
-var chalk  = require('chalk')
-var touch  = require('touch')
-var config = require('../../config')
+var fs         = require('fs')
+var path       = require('path')
+var chalk      = require('chalk')
+var touch      = require('touch')
+var pathToHost = require('../utils/path-to-host')
+var config     = require('../../config')
 
 module.exports = function(args) {
-  var host = args[0] || path.basename(process.cwd())
+  var host = args[0] || pathToHost(process.cwd())
   var file = config.hostsDir + '/' + host + '.json'
   if (fs.existsSync(file)) {
     touch.sync(file)
-    console.log('%s has been successfully reloaded', chalk.cyan(host))
+    console.log(
+      '%s has been successfully stopped (to restart simply go http://%s.ka'),
+      chalk.cyan(host),
+      chalk.cyan(host))
   } else {
     console.log("Can\'t find %s, use katon list", chalk.red(host))
   }
